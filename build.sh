6d86dddf8fa51c97ecb5e589da487d8e90ec0b63
Li Bin <binblee@users.noreply.github.com>
2016-09-29 12:10:53 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-docker build -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:$(git rev-parse --short HEAD) .
-image_id=$(docker build -q -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:$(git rev-parse --short HEAD) .)
+git_hash=$(git rev-parse --short HEAD)
+docker build -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:${git_hash} .
+image_id=$(docker build -q -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:${git_hash})
 docker tag ${image_id} registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:latest
-docker push registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a
+docker push registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:${git_hash}
+docker push registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:latest
