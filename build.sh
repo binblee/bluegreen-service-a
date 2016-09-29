144979a9ac5a639ae7ebdadb748d3dbfcc111916
Li Bin <binblee@users.noreply.github.com>
2016-09-29 11:57:34 +0800

    remove build_number tag from build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 docker build -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:$(git rev-parse --short HEAD) .
 image_id=$(docker build -q -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:$(git rev-parse --short HEAD) .)
-docker tag ${image_id} registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:b${BUILD_NUMBER}
 docker tag ${image_id} registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a:latest
 docker push registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-a
